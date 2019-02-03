93df8046bbfa2682178a3074ceeeaaa089a6bd9a
Jose Subieta <jose.subieta@careerbuilder.com>
2019-02-02 22:03:55 -0400

    Adding all the plumbery for repos and added the student repo with tests

---
+++ Data/Base/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Base
+{
+    public interface IRepository <T>  where T : class
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        IList<T> GetAll();  
+        T GetSingle(Func<T, bool> criteria);
+        IList<T> GetList(Func<T, bool> criteria);
+        void ClearStorage();
+    }
+}
---
+++ Domain/Entities/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Student
+    {
+        public StudentType Type { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+
+    public enum StudentType
+    { 
+        Kinder,
+        High
+    }
+
+    public enum Gender
+    { 
+        M,
+        F
+    }
+}
